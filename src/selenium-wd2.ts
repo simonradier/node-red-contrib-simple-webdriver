96ce0f012ffc8c5e2b0826f83fae269e769a1328
Critik <critikkiller@gmail.com>
2020-09-14 13:23:02 +0200

    Commit for synchro

--- src/selenium-wd2.ts
+++ src/selenium-wd2.ts
@@ -1,13 +1,28 @@
-import { NodeInitializer } from "node-red";
+import { NodeInitializer, NodeAPI, NodeAPISettingsWithData, NodeConstructor, Node, NodeDef } from "node-red";
+import { WebDriver } from "selenium-webdriver";
 
 
 class SeleniumServer {
-    public Setup () {
+    private _driver : WebDriver;
+    private _connected : boolean = false;
+    private _connecting : boolean = false;
+    private _RED : NodeAPI<NodeAPISettingsWithData>;
+
+    public constructor (RED : NodeAPI<NodeAPISettingsWithData>) {
+        //this._driver = new WebDriver();
+        this._RED = RED;
+    }
+
+    public Setup (n : any) : void {
+        let node : Node<any> = <Node<any>> n;
+    }
+
+    public OpenWeb (n : any) : void {
 
     }
 }
 
 export default () : NodeInitializer => (RED) => {
-    let selServ : SeleniumServer = new SeleniumServer();
+    let selServ : SeleniumServer = new SeleniumServer(RED);
 	RED.nodes.registerType("selenium-server", selServ.Setup);
 }
