9243922eea7debf725a0d5dd9cc9242fef4670c9
Simon Radier <simon.radier@outlook.fr>
2020-09-20 22:05:28 +0200

    Refactoring of waitforelement

--- src/nodes/find-element.ts
+++ src/nodes/find-element.ts
@@ -1,7 +1,7 @@
 import { Node, NodeDef, nodes } from "node-red"
 import { By, until, WebDriver } from "selenium-webdriver";
 import { WD2Manager } from "../wd2-manager";
-import { SeleniumMsg, SeleniumNode } from "./node";
+import { SeleniumMsg, SeleniumNode, waitForElement } from "./node";
 
 export interface NodeFindElementDef extends NodeDef, SeleniumNode {
 }
@@ -16,41 +16,35 @@ export function NodeFindElementConstructor (this : NodeFindElement, conf : NodeF
     this.on("input", async (message : any, send, done) => {
         // Cheat to allow correct typing in typescript
         let msg : SeleniumMsg = message;
+        let node = this;
         this.status({});    
         if (msg.driver == null) {
             let error = new Error("Open URL must be call before any other action. For node : " + conf.name);
             this.status({ fill : "red", shape : "ring", text : "error"});
             done(error);
         } else {
-            let waitFor : number = msg.waitFor || conf.waitFor;
-            let timeout : number = msg.timeout || conf.timeout;
-            let target : string = msg.target || conf.target;
-            let selector : string = msg.selector || conf.selector;
-            this.status({ fill : "blue", shape : "dot", text : "waiting for " + (waitFor / 1000).toFixed(1) + " s"});
-            setTimeout (async () => {
-                this.status({ fill : "blue", shape : "dot", text : "locating"});
-                try {
-                    if (target != "") {
-                        //@ts-ignore
-                        msg.element = await msg.driver.wait(until.elementLocated(By[selector](target)), timeout);
+            waitForElement(conf, msg).subscribe ({
+                next (val)  {
+                    if (typeof val === "string") {
+                        node.status({ fill : "blue", shape : "dot", text : val});
+                    } else {
+                        msg.element = val;
                     }
-                    this.status({ fill : "green", shape : "dot", text : "success"})
-                    if (msg.error) { delete msg.error; }
-                    send([msg, null]);
-                    done();
-                } catch (e) {
-                    console.log(e);
-                    msg.error = {
-                        name : conf.name,
-                        selector : selector,
-                        target : target,
-                        value : "catch timeout after " + timeout + " milliseconds for selector type " + selector +  " for  " + target
-                    };
-                    this.status({ fill : "yellow", shape : "dot", text : "location error"})
+                },
+                error(err) {
+                    node.status({ fill : "yellow", shape : "dot", text : "location error"});
+                    msg.error = err;
+                    node.error(err.message);
                     send([null, msg]);
                     done();
+                },
+                complete () {
+                    node.status({ fill : "green", shape : "dot", text : "success"});
+                    if (msg.error) { delete msg.error };
+                    send([msg, null]);
+                    done();
                 }
-            }, waitFor);
+            });
         }
     });
 }
--- src/nodes/node.ts
+++ src/nodes/node.ts
@@ -1,5 +1,6 @@
 import { NodeMessageInFlow } from "node-red__registry";
-import { WebDriver, WebElement } from "selenium-webdriver";
+import { Observable } from "rxjs";
+import { By, until, WebDriver, WebElement } from "selenium-webdriver";
 
 export * from "./open-web";
 export * from "./close-web";
@@ -22,4 +23,39 @@ export interface SeleniumMsg extends NodeMessageInFlow {
     error? : any;
     element? : WebElement;
     webTitle? : string;
+    clickOn? : string;
+}
+
+/**
+ * Wait for the location of an element based on a target & selector.
+ * @param driver A valid WebDriver instance 
+ * @param conf A configuration of a node
+ * @param msg  A node message
+ */
+export function waitForElement(conf : SeleniumNode, msg : SeleniumMsg) : Observable<string | WebElement>{
+    return new Observable<string | WebElement> ((subscriber) => {
+        let waitFor : number = msg.waitFor || conf.waitFor;
+        let timeout : number = msg.timeout || conf.timeout;
+        let target : string = msg.target || conf.target;
+        let selector : string = msg.selector || conf.selector;
+        let element : WebElement;
+        subscriber.next("waiting for " + (waitFor / 1000).toFixed(1) + " s");
+        setTimeout (async () => {
+            try {
+                subscriber.next("locating");
+                if (target != "" && selector != "") {
+                       //@ts-ignore
+                    element = await msg.driver.wait(until.elementLocated(By[selector](target)), timeout);
+                }
+                subscriber.next(element);
+                subscriber.complete();
+            } catch (e) {
+                let error : any = new Error("catch timeout after " + timeout + " milliseconds for selector type " + selector +  " for  " + target);
+                error.selector = selector;
+                error.target = target;
+                console.log(e);
+                subscriber.error(error);
+            }
+        }, waitFor);
+    });
 }
