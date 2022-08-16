dc2eaf10b826567cf905c8768e6cbda0396f7a6c
Simon <simon.dev@outlook.fr>
2022-08-16 12:07:29 +0200

    refactoring of utility fonctions

---
+++ src/utils/red-api.ts
@@ -0,0 +1,13 @@
+import { NodeAPI, NodeAPISettingsWithData } from "node-red"
+
+export class REDAPI {
+    private static instance : NodeAPI<NodeAPISettingsWithData>
+
+    public static get () : NodeAPI<NodeAPISettingsWithData> {
+        return REDAPI.instance
+    }
+
+    public static set (redAPI : NodeAPI<NodeAPISettingsWithData>) : void {
+        REDAPI.instance = redAPI
+    }
+}
---
+++ src/utils/wait-for.ts
@@ -0,0 +1,21 @@
+export class WaitForError extends Error {
+    value : any
+    name : string = "WaitForError"
+}
+
+export async function waitForValue<T>(param: any [], func : (...args) => Promise<T>, expectedValue : T, timeout : number) : Promise<T> {
+    return new Promise<T> ( async (resolve, reject) => {
+        setTimeout(() => {
+            let err = new WaitForError()
+            err.message = "Cannot resolve expected value : " + value
+            err.value = value
+            reject (err)
+        }, timeout)
+
+        let value : T
+        do {
+            value = await func(param)
+        } while (value == expectedValue)
+        resolve(value);
+    })
+}
